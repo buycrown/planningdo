08f88a855563162631a8a6659945298f2649fd8d
buycrown <jagallang@gmail.com>
2026-07-30 11:41:40 +0900

    Add files via upload

---
+++ contents/lcc/config.js
@@ -0,0 +1,2 @@
+// Apps Script 웹앱 URL (2026-07-29 배포 완료)
+window.LCC_CONFIG = { GAS_URL: "https://script.google.com/macros/s/AKfycbxSq_pEBrJ_0u7VSpPQLw663UpZdkkToLj4wQvmRPBlMFXoAC8uLVIPl72MR68t6ASS6g/exec" };
